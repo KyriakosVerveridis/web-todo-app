29565d2c45074a128f7ac9d6921471ae14efdc1c
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-10-07 20:19:18 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import functions
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    print(todo)
+
+todos = functions.get_todos()
+
+
+st.title("My todo App")
+st.subheader("This is my todo app")
+st.write("This is for your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Add new todo",placeholder="Add new todo...",
+              on_change=add_todo,key="new_todo")
+print("Hello")
